fdaf61adb697310682d8982a9bfe698899f3a289
chavan-tushar <tushu01@gmail.com>
2021-09-19 20:50:41 +0100

    Changes 1

---
+++ script.js
@@ -0,0 +1,16 @@
+alert("Hello World!");
+
+let country = "Ireland";
+let continent = "Europe";
+let population = "1.2 Crores";
+
+console.log(`Country is ${country}, it is in ${continent} continent and has a population ${population}`);
+
+// Data Type
+// 1. Number : let myVar = 10; let myVar = 10.45;
+// 2. String : let myVar = "Tushar";
+// 3. Boolean : let myVar = true;
+// 4. Undefined : let myVar;
+// 5. Null : it also represents empty value. 
+// 6. Symbol : Value that is unique and cannot be changed
+// 7. BigInt : Numeric value which is large and cannot be stored in Number. 
