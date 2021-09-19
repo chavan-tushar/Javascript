063dd50c0d25882e37ff3e7788a1cf8111777e29
chavan-tushar <tushu01@gmail.com>
2021-09-19 20:57:18 +0100

    changes 2

--- script.js
+++ script.js
@@ -14,3 +14,4 @@ console.log(`Country is ${country}, it is in ${continent} continent and has a po
 // 5. Null : it also represents empty value. 
 // 6. Symbol : Value that is unique and cannot be changed
 // 7. BigInt : Numeric value which is large and cannot be stored in Number. 
+//
